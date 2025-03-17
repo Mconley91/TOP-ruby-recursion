858c51e8cf61621caf5416dd2c9d12ecd94614d1
mconley91 <matthew.e.conley@gmail.com>
2025-03-17 19:39:07 -0400

    add recursive fib method

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,19 @@
-def fibs(number)
-  arr = [0,1]
+def fibs(number, arr = [0,1])
   while arr.length < number do
     arr << arr[-2] + arr[-1]
   end
   arr
 end
 
-p fibs(8)
+p "#{fibs(8)} Iterative, yay!"
+
+def fibs_rec(num, arr = [0,1])
+  if arr.length < num
+    arr << arr[-2] + arr[-1]
+    fibs_rec(num,arr)
+  else
+    return arr
+  end
+end
+
+p "#{fibs_rec(8)} Recursive, yay!"
