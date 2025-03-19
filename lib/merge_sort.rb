85220ce2eb412ac0014690ea7b04f9ca5add79bd
mconley91 <matthew.e.conley@gmail.com>
2025-03-18 21:28:09 -0400

    add merge_sort

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,12 +1,13 @@
-def merge_sort(arr,accumulator = [])
+def merge_sort(arr)
   if arr.length == 1
     return arr
   else
-    arr1 = merge_sort(arr[..arr.length/2-1],accumulator)
-    arr2 = merge_sort(arr[arr.length/2..],accumulator)
-    p "#{arr1} --&-- #{arr2}"
-    
+    arr1 = merge_sort(arr[..arr.length/2-1])
+    arr2 = merge_sort(arr[arr.length/2..])
+    puts "#{arr1} -&- #{arr2}"
+    return [arr1, arr2].flatten.sort
   end
 end
 
-p "RETURN VALUE: #{merge_sort([3, 2, 1, 13, 8, 5, 0, 1])}"
+puts "Sorted Array: #{merge_sort([3, 2, 1, 13, 8, 5, 0, 1])}"
+puts "Sorted Array: #{merge_sort([105, 79, 100, 110])}"
